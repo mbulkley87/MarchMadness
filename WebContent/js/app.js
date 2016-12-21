6b32a4a1f367b5f7266585624e9d7d2a5933131c
Matt Bulkley <matthew.bulkley@gmail.com>
2016-12-20 23:23:04 -0500

    Basic Login Working

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -10,11 +10,15 @@
 	};
 	firebase.initializeApp(config);
 	//Get Elements
-	var txtEmail = document.getElementById("txtEmail");
-	var txtPassword = document.getElementById("txtPassword");
+	var txtEmail = document.getElementById("inputUsernameEmail");
+	var txtPassword = document.getElementById("inputPassword");
 	var btnLogin = document.getElementById("btnLogin");
 	var btnSignUp = document.getElementById("btnSignUp");
 	var btnLogout = document.getElementById("btnLogout");
+	var userEntry = document.getElementById("userEntry");
+	var passwordEntry = document.getElementById("passwordEntry");
+	var loggedInAs = document.getElementById("loggedInAs");
+	var userNameField = document.getElementById("userNameField");
 	
 	//Add Login Event
 	btnLogin.addEventListener('click', e => {
@@ -40,15 +44,22 @@
 	firebase.auth().onAuthStateChanged(firebaseUser => {
 		if (firebaseUser) {
 			console.log(firebaseUser);
+			userNameField.innerHTML = firebaseUser.email;
 			btnLogout.classList.remove('hide');
+			loggedInAs.classList.remove('hide');
 			btnLogin.classList.add('hide');
 			btnSignUp.classList.add('hide');
+			userEntry.classList.add('hide');
+			passwordEntry.classList.add('hide');
 		}
 		else {
 			console.log("not logged in");
 			btnLogout.classList.add('hide');
+			loggedInAs.classList.add('hide');
 			btnLogin.classList.remove('hide');
 			btnSignUp.classList.remove('hide');
+			userEntry.classList.remove('hide');
+			passwordEntry.classList.remove('hide');
 		}
 	});
 	
